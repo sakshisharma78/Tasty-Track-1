d2e8396fa9d976d151053c160b754191eff53d93
sakshi sharma <122162031+sakshisharma78@users.noreply.github.com>
2024-09-02 08:56:49 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
+        await mongoose.connect("mongodb://localhost:27017/tastytrack", {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
